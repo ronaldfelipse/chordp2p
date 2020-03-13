ef8c50f1629b180effecd07a713b15e491bfe160
ronaldfelipse <ronald-1524@hotmail.com>
2020-03-13 00:08:51 -0500

    Correcion bug asignacion antecesor

--- Servidor.py
+++ Servidor.py
@@ -2,7 +2,6 @@ import zmq
 import random
 import hashlib
 import os
-from uuid import getnode as get_mac
 
 IDInit = 0
 IDFin = 0
@@ -12,8 +11,8 @@ MyPort = ""
 
 ServerID = ""
 
-PredecesorIP = ""
-PredecesorPort = ""
+AntecesorIP = ""
+AntecesorPort = ""
 
 context = zmq.Context()
 socketEscucha = context.socket(zmq.REP)
@@ -69,8 +68,8 @@ def inToAnillo():
     global MyPort
     global IDFin
     global IDInit
-    global PredecesorIP
-    global PredecesorPort
+    global AntecesorIP
+    global AntecesorPort
 
     FirstIP = input("INGRESE LA IP DE ALGUN NODO\n")
     FirstIP = "localhost"
@@ -84,13 +83,10 @@ def inToAnillo():
         else:
             if (Bdecode(MSJData[1]) == "1"):
                 IDInit =   Bdecode(MSJData[2])
-                PredecesorIP = FirstIP
-                PredecesorPort = FirstPort
+                AntecesorIP = Bdecode(MSJData[3])
+                AntecesorPort = Bdecode(MSJData[4])
                 break
             else:
-
-                if Bdecode(MSJData[2]) == "":
-                    print("EstaMierda")
 
                 FirstIP = Bdecode(MSJData[2])
                 FirstPort = Bdecode(MSJData[3])
@@ -103,6 +99,7 @@ def FirstConect():
     global IDInit
     global IDFin
     global ServerID
+    
 
     if not CheckFirstNode():
 
@@ -134,7 +131,6 @@ def getID():
     ServerID = Random
     hash  = hashlib.new("sha1",Strencode(Random))
     ServerID = int(hash.hexdigest(), 16)
-
     print("ServerID: "+str(ServerID))
 
 def CheckNewID(NewServerID,IP,PORT):
@@ -142,16 +138,16 @@ def CheckNewID(NewServerID,IP,PORT):
     global IDInit
     global IDFin
 
-    global PredecesorIP
-    global PredecesorPort
+    global AntecesorIP
+    global AntecesorPort
+    
+    global MyIP
+    global MyPort
 
     global ServerID
 
     if Checkintervale(IDInit,IDFin,NewServerID) :
-        if PredecesorIP == "":
-            PredecesorIP    = IP
-            PredecesorPort  = PORT
-
+                    
         IdInicopy = IDInit
 
         if int(NewServerID) == 2**160 :
@@ -159,15 +155,24 @@ def CheckNewID(NewServerID,IP,PORT):
         else:
             IDInit = int(NewServerID)+1
 
-        PrintMyRange()
-        return [b"1",b"1",Strencode(IdInicopy)]
+        if AntecesorIP ==  "":
+             respt = [b"1",b"1",Strencode(IdInicopy),Strencode(MyIP),Strencode(MyPort)]
+       
+        else:
+             respt = [b"1",b"1",Strencode(IdInicopy),Strencode(AntecesorIP),Strencode(AntecesorPort)]
+       
+        AntecesorIP    = IP
+        AntecesorPort  = PORT
+        PrintMyRange()   
+        return respt
     else:
-        return [b"1",b"2",Strencode(PredecesorIP),Strencode(PredecesorPort)]
+        return [b"1",b"2",Strencode(AntecesorIP),Strencode(AntecesorPort)]
 
 def PrintMyRange():
 
     global IDInit
     global IDFin
+    global AntecesorPort
 
     print("-----------------------------------------------------")
     print("IDIni = " + str(IDInit))
@@ -179,8 +184,8 @@ def TypeUpload(content,hashkey):
     global PathFile
     global IDInit
     global IDFin
-    global PredecesorIP
-    global PredecesorPort
+    global AntecesorIP
+    global AntecesorPort
 
     hashPart  = hashlib.new("sha1",content)
     if (hashkey != hashPart.hexdigest() ) :
@@ -195,7 +200,7 @@ def TypeUpload(content,hashkey):
             archivo.close()
             return [b"1",b"1",Strencode(hashkey)]
         else:
-            return [b"1",b"2",Strencode(PredecesorIP),Strencode(PredecesorPort)]
+            return [b"1",b"2",Strencode(AntecesorIP),Strencode(AntecesorPort)]
 
 
 
